222b4f2c69517ecbcaf6b3b496d681eb7fcdf8ef
Erika <Erika.lundblad95@gmail.com>
2026-05-08 11:55:29 +0200

    so close med listgraph kom igen

--- backend/src/main/java/se/su/inlupp/ListGraph.java
+++ backend/src/main/java/se/su/inlupp/ListGraph.java
@@ -23,27 +23,10 @@ public class ListGraph<T> implements Graph<T> {
     if (!graphMap.containsKey(node)) {
       throw new NoSuchElementException();
     }
-    //Kanterna måste tas bort, även kanten som pekar mot T node måste bort.
-  /*  Collection <Set<EdgeClass<T>>> edges = graphMap.values();
-    Iterator <Set<EdgeClass<T>>> iter = edges.iterator();
-    while (iter.hasNext()){
-      Set<EdgeClass<T>> checkNode = iter.next();
-      //for(EdgeClass<T> e : edges){}
-      if(checkNode.contains(node)) {
-        iter.remove();
-      }
+   for(Set<EdgeClass<T>> e: graphMap.values()){
+    e.removeIf(edges -> edges.getDestination().equals(node));
+
     }
-
-   for(Set<EdgeClass<T>> e: edges){
-     for(EdgeClass<T> ec : e){
-
-       graphMap.get(ec).removeIf(ec -> ec.getDestination().equals(node)); 
-       }
-      }
-    */ 
-
-
-
     graphMap.remove(node);
 
   }
@@ -58,7 +41,8 @@ public class ListGraph<T> implements Graph<T> {
     if (weight < 0) {
       throw new IllegalArgumentException();
     }
-    if (graphMap.get(node1).contains(node2) || graphMap.get(node2).contains(node1))
+    if(getEdgeBetween(node1, node2) != null)
+
       throw new IllegalStateException();
     try {
       this.add(node1);
@@ -73,6 +57,7 @@ public class ListGraph<T> implements Graph<T> {
     bEdges.add(new EdgeClass<T>(node1, name, weight));
   }
 
+
   @Override
   public void disconnect(T node1, T node2) {
     if (!graphMap.containsKey(node1) || !graphMap.containsKey(node2)) {
@@ -83,10 +68,21 @@ public class ListGraph<T> implements Graph<T> {
       throw new IllegalStateException();
     }
 
-    graphMap.get(node1).remove(node2);
-    graphMap.get(node2).remove(node1);
-
-    // throw new UnsupportedOperationException("Unimplemented method 'disconnect'");
+    
+    EdgeClass<T> e1 = getEdgeBetween(node1, node2);
+    EdgeClass<T> e2 = getEdgeBetween(node2, node1);
+    
+    for(EdgeClass<T> e: graphMap.get(node1)){
+      if(e.equals(e1)){
+        graphMap.remove(e);
+      }
+    }
+    for(EdgeClass<T> e: graphMap.get(node2)){
+      if(e.equals(e2)){
+        graphMap.remove(e);
+      }
+    }
+      //SO CLOSE men lite kvar?
   }
 
   @Override
